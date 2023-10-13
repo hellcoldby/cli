e865f78c7a27df376afd697250a7ac2487fbe725
unknown <guopingyang@tecent.com>
2023-10-13 16:51:35 +0800

    test

--- templates/src/main.tsx
+++ templates/src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "tdesign-react/es/style/index.css";
 import { BrowserRouter } from "react-router-dom";
 i
 
